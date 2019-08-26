f927f1b636d51b44de3c5e4da0d28133f1598e11
t-sin <shinichi.tanaka45@gmail.com>
2019-08-26 16:51:01 +0900

    CPU; add registers

---
+++ cpu.lisp
@@ -0,0 +1,30 @@
+(defpackage #:lambdaboy.cpu
+  (:use #:cl)
+  (:export))
+(in-package #:lambdaboy.cpu)
+
+(defstruct cpu
+  ;;; internal states
+
+  ;;; registers
+  ;;; this would be changed
+
+  ;; 8-bit registers pair can be used as one 16-bit register
+  ;; it should be a 16-bit bit vector?
+
+  ;; 8-bit registers set #1
+  a f  ; accumlator & flag register
+  b c d e h l  ; general purpose registers
+
+  ;; 8-bit registers set #2
+  a* f*  ; accumlator & flag register
+  b* c* d* e* h* l*  ; general purpose registers
+
+  ;; special purpose registers
+  i  ; interrupt page access register
+  r  ; memory refresh register
+  ix  ; index register #1
+  iy  ; index register #2
+  sp  ; stack pointer
+  pc  ; program counter
+)
