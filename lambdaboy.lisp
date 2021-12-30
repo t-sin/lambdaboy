d214e0b43c653dd107306c5e1056c983b00126f1
t-sin <shinichi.tanaka45@gmail.com>
2021-12-30 17:49:30 +0900

    Fix syntax error

--- lambdaboy.lisp
+++ lambdaboy.lisp
@@ -254,7 +254,7 @@
                       (let ((result (+ (register-hl reg) (register-bc reg))))
                         (setf (register-a reg) result)
                         (set-flags (zerop result) nil (> result #x0f) (> result #xff))
-                      1)
+                      1))
                 (#x21 (let ((d16 (8bit->16bit (operand-1) (operand-2))))
                         (log-op "LD HL, #x~x" d16)
                         (setf (register-hl reg) d16))
