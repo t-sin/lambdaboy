d2887db6a2acac4d1d0614e91903703626d15f38
t-sin <shinichi.tanaka45@gmail.com>
2022-01-30 18:51:44 +0900

    Add some macros to generate varjo's compiled-stages

--- window.lisp
+++ window.lisp
@@ -1,5 +1,32 @@
 ; (ql:quickload '("cl-opengl" "cl-glfw3" "varjo"))
 
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun collect-normal-args (args)
+    (loop
+      :for arg :in args
+      :until (eq arg '&uniform)
+      :collect arg))
+
+  (defun collect-uniforms (args)
+    (loop
+      :with uniform? := nil
+      :for arg :in args
+      :if uniform?
+      :collect arg
+      :do (when (eq arg '&uniform)
+            (setf uniform? t))))
+
+  (defmacro defshader (kind (&body args) &body body)
+    (let ((input (collect-normal-args args))
+          (uniforms (collect-uniforms args)))
+      `(varjo:make-stage ,kind ',input ',uniforms '(:460) ',`(,@body))))
+
+  (defmacro defvertex ((&body args) &body body)
+    `(defshader :vertex (,@args) ,@body))
+
+  (defmacro deffragment ((&body args) &body body)
+    `(defshader :fragment (,@args) ,@body)))
+
 (defun setup ()
   '(:title "test window"
     :width 800
